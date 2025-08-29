a9d5bc13d1a18d328eb974ef377c8f6f2496068f
victor hugo moreira brito <moreiravictorbrito@gmail.com>
2025-08-29 14:13:33 +0000

    mudança

--- 2025.2/B/dump1.c
+++ 2025.2/B/dump1.c
@@ -6,10 +6,10 @@ void dump (void *p, int n) {
         printf("%p - %02x\n", p1, *p1);
             p1++;
               }
-              }
-
-              int main (void) {
-                int i = 10000;
-                  dump(&i, sizeof(i));
-                    return 0;
-                    }
+              
+}
+int main (void) {
+  int i = 10000;
+  dump(&i, sizeof(i));
+  return 0;
+}
